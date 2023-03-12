636b905197b08f2d41d9785d06fc4572bff5ad4d
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-12 18:21:59 +0300

    Задание 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+int num = GetNumberFromUser("Введите длину массива: ", "Ошибка ввода!");
+int[] array = GetArray(num, 100, 999);
+int evenCount = GetEvenCount(array);
+Console.WriteLine($"[{String.Join(", ", array)}] -> в массиве {evenCount} четных чисел");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetEvenCount(int[] arr)
+{
+    int count = 0;
+    foreach (int el in arr)
+    {
+        if (el % 2 == 0) count++;
+    }
+    return count;
+}
